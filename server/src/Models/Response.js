4798e9dd3cbb909d6a9a6fc9dcbb44b09c84d92a
LizanoLeo123 <leoln123@live.com>
2020-11-15 18:21:19 -0600

    Deploy branch

--- server/src/Models/Response.js
+++
@@ -1,9 +0,0 @@
-// class constructor
-function Response(message, data, times) {
-    this.message = message;
-    this.data = data;
-    this.times = times;
-}
-
-// export the class
-module.exports = Response;
